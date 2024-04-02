d554efd4129ff5cdacbb04251bbcbde210b2758e
Valtteri Sutelainen <valtteri.n.sutelainen@tuni.fi>
2024-04-02 04:51:28 +0300

    Removed icon variable from script.js (was left behind the old button versio) This fixes the error from switching between themes

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-
-
 function ThemeToggle()
 {
 
     var body = document.body;
-    var icon = document.querySelector('.btn');
+
     
     // Check the current state before it's toggled
     var isDarkMode = body.classList.contains('dark-mode');
@@ -12,11 +10,5 @@ function ThemeToggle()
     // Now toggle the class
     body.classList.toggle('dark-mode');
 
-    // Set the icon based on the previous state
-    if (isDarkMode) {
-        icon.textContent = 'Light mode';
-    } else {
-        icon.textContent = 'Dark mode';
-    }
 
 }
