341aebacb9c32923951d167a3b204560536b296f
Valtteri Sutelainen <valtteri.n.sutelainen@tuni.fi>
2024-11-16 03:02:14 +0200

    updated styling added content in skills

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function ThemeToggle()
 
     var body = document.body;
 
-    
     // Check the current state before it's toggled
     var isDarkMode = body.classList.contains('dark-mode');
 
@@ -12,3 +11,14 @@ function ThemeToggle()
 
 
 }
+
+
+window.addEventListener('load', function() {
+    document.getElementById('toggle2').checked = false;
+  });
+
+
+  document.getElementById('scrollToTopBar').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default anchor behavior
+    document.getElementById('topBar').scrollIntoView({ behavior: 'smooth' });
+});
